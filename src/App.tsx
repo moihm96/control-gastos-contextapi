1fe775a9e44ca2341ee77141a68ea82edd4fe3fc
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-05-03 10:59:16 +0100

    Filtered expenses by categories

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
 import ExpenseModal from "./components/ExpenseModal";
 import ExpenseList from "./components/ExpenseList";
+import FilterByCategory from "./components/FilterByCategory";
 
 function App() {
   const { state, dispatch } = useBudget();
@@ -40,6 +41,7 @@ function App() {
 
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
+          <FilterByCategory />
           <ExpenseList />
           <ExpenseModal />
         </main>
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { DraftExpense, Expense } from "../types";
+import { Category, DraftExpense, Expense } from "../types";
 
 export type BudgetActions =
   | { type: "add-budget"; payload: { budget: number } }
@@ -11,13 +11,15 @@ export type BudgetActions =
   | { type: "update-expense"; payload: { expense: Expense } }
   | {
       type: "restart-app";
-    };
+    }
+  | { type: "add-filter-category"; payload: { id: Category["id"] } };
 
 export type BudgetState = {
   budget: number;
   modal: boolean;
   expenses: Expense[];
   editingId: Expense["id"];
+  currentCategory: Category["id"];
 };
 
 const initialBudget = (): number => {
@@ -35,6 +37,7 @@ export const initialState: BudgetState = {
   modal: false,
   expenses: localStorageExpenses(),
   editingId: "",
+  currentCategory: "",
 };
 
 const createExpense = (draftExpense: DraftExpense): Expense => {
@@ -93,6 +96,12 @@ export const budgetReducer = (state: BudgetState, action: BudgetActions) => {
       ...state,
       budget: 0,
       expenses: [],
+    };
+  }
+  if (action.type === "add-filter-category") {
+    return {
+      ...state,
+      currentCategory: action.payload.id,
     };
   }
   return state;
