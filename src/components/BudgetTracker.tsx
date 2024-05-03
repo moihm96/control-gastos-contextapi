769c33b9d6e701bb01c4f661c27b6a3021b94c93
Moises Heredia Molina <moises.molina@karnovgroup.com>
2024-05-03 10:37:47 +0100

    Percentage Circular progressbar

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,32 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import { useMemo } from "react";
 
 export default function BudgetTracker() {
   const { state, totalExpenses, remainingBudget } = useBudget();
 
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+
+  const colorPercentage = useMemo(
+    () => (percentage === 100 ? "#DC2626" : "#3b82f6"),
+    [percentage]
+  );
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="grafico" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: colorPercentage,
+            trailColor: "#F5F5F5",
+            textSize: 8,
+            textColor: colorPercentage,
+          })}
+          text={`${percentage}% Gastado`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
